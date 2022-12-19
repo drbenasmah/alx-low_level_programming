a765b70c187e1e44bf6b1ca62d7b12bcc61dde4a
drbenasmah <drbenasmah2016@gmail.com>
2022-12-19 12:05:18 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *  * reset_to_98 - update value to 98
+ *   *
+ *    * @n: pointer
+ *     */
+
+void reset_to_98(int *n)
+{
+		*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers array and strings
