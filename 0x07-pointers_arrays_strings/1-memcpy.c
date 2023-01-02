9ba6051c3e04cdc1b9256a46cbe2672fefda2331
drbenasmah <drbenasmah2016@gmail.com>
2023-01-02 11:03:13 -0800

    task 2 to advance

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,15 +10,16 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-    int size = n;
+int size = n;
 
-    if (size > 0)
-    {
-								   int i;
-					  
-								   for (i = 0; i < size; i++)
+if (size > 0)
+{
+						 
+int i;					  
+								  for (i = 0; i < size; i++)
 								   dest[i] = src[i];
-								  }
+							
+}
 
-    return (dest);
+return (dest);
 }
