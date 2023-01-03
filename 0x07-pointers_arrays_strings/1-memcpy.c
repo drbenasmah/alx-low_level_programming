a3a2221d691c40d100aa92b62fcc763c1e648a4a
drbenasmah <drbenasmah2016@gmail.com>
2023-01-02 18:09:53 -0800

    cor

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -14,12 +14,14 @@ int size = n;
 
 if (size > 0)
 {
-						 
-int i;					  
-								  for (i = 0; i < size; i++)
-								   dest[i] = src[i];
-							
+
+int i;
+
+for (i = 0; i < size; i++)
+{    dest[i] = src[i];
+
 }
 
 return (dest);
+
 }
