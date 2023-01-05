83da782de2c6c687f5294baecfef5ebf73f259c1
drbenasmah <drbenasmah2016@gmail.com>
2023-01-05 03:38:46 -0800

    betty correc

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -6,27 +6,27 @@
  * * @s: string to search
  * * @accept: target matches
  * * Return: pointer to index of string at first occurence
- * */
+ */
 
 char *_strpbrk(char *s, char *accept)
 {
 		int x = 0, y;
 
 			while (s[x] != '\0')
+			{
+				for (y = 0; accept[y] != '\0'; y++)
+				{
+					if (s[x] == accept[y])
 					{
-								for (y = 0; accept[y] != '\0'; y++)
-											{
-															if (s[x] == accept[y])
-																			{
-																								s = &s[x];
-																												return (s);
-																															}
+						s = &s[x];
+						return (s);
+					}
 
-																	}
+				}
 
-									x++;
-										}
+				x++;
+			}
 
-				return (NULL);
+			return (NULL);
 
 }
