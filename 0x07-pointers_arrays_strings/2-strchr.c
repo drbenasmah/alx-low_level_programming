0fe5673609a62243bdf571da15fe9c1eb0d8a8e2
drbenasmah <drbenasmah2016@gmail.com>
2023-01-05 04:48:31 -0800

    task 0 Betty cor

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,14 @@
 
 char *_strchr(char *s, char c)
 {
-		int i = 0;
+	int i = 0;
 
-			while (s[i] != '\0' && s[i] != c)
-						i++;
+	while (s[i] != '\0' && s[i] != c)
+		i++;
 
-				if (s[i] == c)
-							return (&s[i]);
+	if (s[i] == c)
+		return (&s[i]);
 
-					else
-								return (NULL);
+	else
+		return (NULL);
 }
