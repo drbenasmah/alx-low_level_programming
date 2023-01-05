f2779e9f572c20b5bec3fad15f5f754dfcb37fcf
drbenasmah <drbenasmah2016@gmail.com>
2023-01-05 02:52:11 -0800

    cor b

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -5,20 +5,20 @@
  * * @src: source
  * * @n: number of bytes
  * * Return: pointer to dest
- * */
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
 		int size = n;
 
-			if (size > 0)
-					{
-								int i;
+		if (size > 0)
+		{
+			int i;
 
-										for (i = 0; i < size; i++)
-													dest[i] = src[i];
-											}
+			for (i = 0; i < size; i++)
+				dest[i] = src[i];
+		}
 
-				return (dest);
+		return (dest);
 }
