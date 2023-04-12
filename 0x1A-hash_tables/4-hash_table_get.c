57952976354abb5b9d9e492b3491f2cc9fb0c8ca
drbenasmah <drbenasmah2016@gmail.com>
2023-04-13 01:45:05 +0200

    task 4-5 update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,30 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves the value associated with a key in a hash table.
- * @ht: A pointer to the hash table.
- * @key: The key to get the value of.
- *
- * Return: If the key cannot be matched - NULL. Otherwise - the value associated with key in ht.
+ * hash_table_get - Retrieve the value associated with a key in a hash table.
+ * @ht: Pointer to the hash table.
+ * @key: Key to get the value of.
+ * Return: If key not found - NULL.
+ * Otherwise - value associated with key in ht.
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    hash_node_t *node;
-    unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-    if (ht == NULL || key == NULL || *key == '\0') {
-        return NULL;
-    }
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    if (index >= ht->size) {
-        return NULL;
-    }
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-    node = ht->array[index];
-    while (node != NULL && strcmp(node->key, key) != 0) {
-        node = node->next;
-    }
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-    return (node == NULL) ? NULL : node->value;
+	return ((node == NULL) ? NULL : node->value);
 }
--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,33 +1,40 @@
 #include "./hash_tables.h"
 
 /**
- * hash_table_print - Prints the key-value pairs of a hash table.
- * @ht: A pointer to the hash table to print.
+ * hash_table_print - Prints a hash table.
+ * @ht: Pointer to the hash table to print.
  *
- * Description: Key-value pairs are printed in the order they appear in
- *              the array of the hash table.
+ * Description: Key/value pairs are printed in
+ * the order they appear in the array
+ *              of the hash table.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-    hash_node_t *node;
-    unsigned long int i;
-    unsigned char first_pair = 1;
+	hash_node_t *node;
+	unsigned long int i;
+	unsigned char comma_flag = 0;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            if (first_pair == 0)
-                printf(", ");
-            printf("'%s': '%s'", node->key, node->value);
-            first_pair = 0;
-            node = node->next;
-        }
-    }
-    printf("}\n");
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (comma_flag == 1)
+				printf(", ");
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
+		}
+	}
+	printf("}\n");
 }
