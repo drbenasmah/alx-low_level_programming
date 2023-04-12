26f88ee558d0d890b288abc3241276e5e5b0dca3
drbenasmah <drbenasmah2016@gmail.com>
2023-04-13 01:16:21 +0200

    task 0 update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -9,12 +9,12 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+unsigned long int hash;
+int c;
 
-    hash = 5381;
-    while ((c = *str++) != 0)
-    hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+hash = 5381;
+while ((c = *str++) != 0)
+hash = ((hash << 5) + hash) +c; /* hash * 33 + c */
 
-    return (hash);
+return (hash);
 }
